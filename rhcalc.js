b9cd9b182cf11cd011a3e952aea2e93fd78c4f4b
Muchen He <mansurhe@outlook.com>
2017-11-05 21:36:31 -0800

    Added denominator input

--- rhcalc.js
+++ rhcalc.js
@@ -1,5 +1,3 @@
-var order = 1;
-
 jQuery(document).ready(function ($) {
 });
 
@@ -8,18 +6,43 @@ function systemOrderChangeEvent() {
     var $sysOrderText = $("#sys-order-text")
     var order = $sysOrderInput.val();
 
-    switch (order % 10) {
-        case 1:
-            $sysOrderText.html('-st');
-            break;
-        case 2:
-            $sysOrderText.html('-nd');
-            break;
-        case 3:
-            $sysOrderText.html('-rd');
-            break;
-        default:
-            $sysOrderText.html('-th');
-            break;
+    if (order > 0 && order <= 11) {
+
+        switch (order % 10) {
+            case 1:
+                $sysOrderText.html('-st');
+                break;
+            case 2:
+                $sysOrderText.html('-nd');
+                break;
+            case 3:
+                $sysOrderText.html('-rd');
+                break;
+            default:
+                $sysOrderText.html('-th');
+                break;
+        }
+
+        $("#denom_input").html(regenDenomInputHTML(order));
     }
+}
+
+function regenDenomInputHTML(order) {
+    htmlstr = '';
+
+    for (var i = order; i > 0; i--) {
+        htmlstr += '<input id="denom_coeff_' + i + '" class="coeff-input" type="number" value="0">';
+        htmlstr += '<span>s</span>';
+
+        if (order > 1) {
+            htmlstr += '<sup>' + i + '</sup>'
+        }
+
+        htmlstr += '+';
+    }
+
+    // Constant term
+    htmlstr += '<input id="denom_coeff_0" class="coeff-input" type="number" value="0">';
+
+    return htmlstr
 }
