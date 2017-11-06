c1b871aa9585ae4cd656fa8d302b14e457dc86aa
Muchen He <mansurhe@outlook.com>
2017-11-05 22:24:55 -0800

    Done some stuff for javascript; need to find a way to somehow parse K

--- rhcalc.js
+++ rhcalc.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function ($) {
+    $('#compute-rh').on('click', function() {
+        RH();
+    });
 });
 
 function systemOrderChangeEvent() {
@@ -45,4 +48,61 @@ function regenDenomInputHTML(order) {
     htmlstr += '<input id="denom_coeff_0" class="coeff-input" type="number" value="0">';
 
     return htmlstr
+}
+
+function RH() {
+    // Get order
+    var order = $("#sys-order").val();
+
+    // Check that the error is correct
+    if (order < 1 || order > 11) {
+        console.error("Error: wrong order");
+        return;
+    }
+
+    // Obtain all coefficients and put into an array
+    var coeff = getCoefficients(order);
+    if (coeff == undefined) return;
+
+    // Check if all coefficient are non-zero
+    if (checkAllZero(coeff)) {
+        console.warn("All denominators have 0 as coefficients; system is naturally unstable");
+        return;
+    }
+
+    // do some more RH stuff
+
+
+    console.log(coeff);
+}
+
+function getCoefficients(order) {
+    var coeff = []
+    for (var i = order; i >= 0; i--) {
+        var ci = parseFloat($('#denom_coeff_' + i).val());
+
+        // check that every entry is valid
+        if (ci != undefined) {
+            coeff.push(ci);
+        } else {
+            console.error("Error: coefficient undefined");
+            return undefined;
+        }
+    }
+
+    return coeff;
+}
+
+function checkAllZero(coeff) {
+    for (var i = 0, l = coeff.length; i < l; i++) {
+        if (coeff[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// returns a lower bound and upper bound for k
+function computeRH(coeff) {
+
 }
